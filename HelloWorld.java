54d2ae3711fc748551e1e2b60809780b5e092694
Chad Whiteley <62949050+ComputerScienceTeacher@users.noreply.github.com>
2025-06-18 13:37:52 +0000

    Just showing you how to do this.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("this is what you need to do.");
+        System.out.println("This is a simple Java program.");
+        System.out.println("Make sure to fix the typo in the previous line.");
+        System.out.println("Goodbye, World!");
+        // Note: The line with "Systen.out.println" contains a typo and should be corrected to "System.out.println".
+        System.out.println("This line is added to ensure the program runs correctly.");
+        System.out.println("End of the program.");
+        // The program prints a series of messages to the console.
+        System.out.println("Thank you for running this program!");
+        System.out.println("Remember to always check your code for typos.");
+        System.out.println("This is the last line of output.");
+        System.out.println("Have a great day!");
+        System.out.println("This is a simple Java program demonstrating output.");
+        System.out.println("Feel free to modify and experiment with the code.");
+        System.out.println("This is a demonstration of a Java program.");
+        System.out.println("You can add more lines to customize the output.");
+        System.out.println("This is a basic example of Java syntax.");
     }
 }
