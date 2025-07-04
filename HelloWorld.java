42e2017d305cb05277a29d83ca93539738e1b5d3
Chad Whiteley <62949050+ComputerScienceTeacher@users.noreply.github.com>
2025-07-04 23:10:53 +0000

    Committing HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,10 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         System.out.println("Hello, World!");
-        System.out.println("this is what you need to do.");
-        System.out.println("This is a simple Java program.");
-        System.out.println("Make sure to fix the typo in the previous line.");
-        System.out.println("Goodbye, World!");
-        // Note: The line with "Systen.out.println" contains a typo and should be corrected to "System.out.println".
-        System.out.println("This line is added to ensure the program runs correctly.");
-        System.out.println("End of the program.");
-        // The program prints a series of messages to the console.
-        System.out.println("Thank you for running this program!");
-        System.out.println("Remember to always check your code for typos.");
-        System.out.println("This is the last line of output.");
-        System.out.println("Have a great day!");
-        System.out.println("This is a simple Java program demonstrating output.");
-        System.out.println("Feel free to modify and experiment with the code.");
-        System.out.println("This is a demonstration of a Java program.");
-        System.out.println("You can add more lines to customize the output.");
-        System.out.println("This is a basic example of Java syntax.");
+        System.out.println(" My name is Emelyn Moran.");
+        System.out.println("My favorite food is pizza");
+        System.out.println("I would like to meet The Weeknd");
+        System.out.println("my least favorite chore is washing dishes.");
+        System.out.println("I would rather be traveling.");
+       }
     }
-}
